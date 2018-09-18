41d624a946249edd7d2833b2dcc47bbb1e2725d8
Ruth Diaz <aby.r_27@hotmail.com>
2018-09-18 17:23:21 -0600

    Nuevos cambios

--- java/menu.js
+++ java/menu.js
@@ -1,21 +1,31 @@
-$(document).ready(main);
-
-var contador = 1;
-
-function main(){
-	$('.menu_bar').click(function(){
-		//$('nav').toggle(); mas directo el menu
-
-		if(contador == 1){
-			$('nav').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left: '-100%'
-			});
-		}
-	});
-};
+ $(document).ready(main);
+ var contador = 1;
+ function main(){
+ 	$('.menu_bar').click(function(){
+ 		//$('nav').toggle(); mas directo el menu
+ 		if(contador == 1){
+ 			$('nav').animate({
+ 				left: '0'
+ 			});
+ 			contador = 0;
+ 		} else {
+ 			contador = 1;
+ 			$('nav').animate({
+ 				left: '-100%'
+ 			});
+ 		}
+ 	});
+ };
+// $(document).ready(main);
+//  function main(){
+// 	$('.menu_bar').click(function(){
+// 		//console.log('El boton esta funcionando');
+// 		var menu = document.getElementsByClassName('desaparece');
+// 		for(var i = 0; i < menu.length; i++){
+// 			menu(i).classList.toggle('desaparece');
+// 		 	$('nav').animate({
+// 		 		left: '-100%'
+// 		 	});
+// 		 };
+// 	});
+//  };
